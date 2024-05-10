74289399151cd65fb90ed10829c653e582c36c81
AnhKhoa-dev <nguyenkhoa.offical2021@gmail.com>
2024-05-10 23:13:18 +0700

    Khoa: Edit Home page 10052024

---
+++ bookingdoctor_web/src/components/UI/Home/HomeBlog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeBlog = () => {
+  return (
+    <section className='container'>
+        <div className="row">
+            <p>Thể hiện tin tức trong này</p>
+        </div>
+    </section>
+  )
+}
+
+export default HomeBlog
