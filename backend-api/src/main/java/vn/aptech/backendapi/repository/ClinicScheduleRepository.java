6c0d51ad4a62710856a093ae477506661d5aa422
AnhKhoa-dev <nguyenkhoa.offical2021@gmail.com>
2024-05-20 10:28:51 +0700

    add clinic

--- backend-api/src/main/java/vn/aptech/backendapi/repository/ClinicScheduleRepository.java
+++ backend-api/src/main/java/vn/aptech/backendapi/repository/ClinicScheduleRepository.java
@@ -1,2 +1,7 @@
-package vn.aptech.backendapi.repository;public interface ClinicScheduleRepository {
+package vn.aptech.backendapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vn.aptech.backendapi.entities.ClinicSchedule;
+
+public interface ClinicScheduleRepository extends JpaRepository<ClinicSchedule, Integer> {
 }
