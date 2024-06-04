6a189d574396f09ce79460f764a322a7ac22cd33
AnhKhoa-dev <nguyenkhoa.offical2021@gmail.com>
2024-06-04 20:46:31 +0700

    04/06/2024 edit app mobile

--- mobile/lib/routes/route.dart
+++ mobile/lib/routes/route.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:mobile/screens/doctor/payment_screen.dart';
 
 //import '../screens/home/home_screen.dart';
 import '../screens/auth/login_screen.dart';
 import '../screens/auth/register_screen.dart';
 import '../screens/auth/signin_screen.dart';
+import '../screens/doctor/booking_patient_screen.dart';
+import '../screens/doctor/doctor_booking.dart';
 import '../screens/doctor/doctor_detail_screen.dart';
 import '../widgets/navigation_menu.dart';
 
@@ -16,6 +19,9 @@ class Routes {
       '/register': (context) => const RegisterScreen(),
       '/sign-in': (context) => const SignInScreen(),
       '/doctor': (context) => const DoctorDetailScreen(),
+      '/doctor/booking': (context) => const DoctorBookingScreen(),
+      '/doctor/booking/patient': (context) => const DoctorBookingPatientScreen(),
+      '/doctor/booking/payment': (context) => const PaymentScreen(),
     };
   }
 }
